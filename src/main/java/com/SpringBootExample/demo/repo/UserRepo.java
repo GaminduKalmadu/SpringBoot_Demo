ff3176c07b60a7f2b2cc0f3442dd262b9241f6f2
Gamindu Kalmadu <gamindukalmadu8@gmail.com>
2026-06-14 21:05:32 +0530

    user repo file

---
+++ src/main/java/com/SpringBootExample/demo/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.SpringBootExample.demo.repo;
+
+import com.SpringBootExample.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
